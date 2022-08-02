2389581f905030f16c1d6fbbaa19a88a6aa7d00f
dmurchison <duncancmurchison@gmail.com>
2022-08-02 13:54:19 -0400

    workin

--- frontend/components/question/question_form.jsx
+++ frontend/components/question/question_form.jsx
@@ -1,21 +1,25 @@
 import React, { useState, useEffect } from 'react';
-import reactMarkdown from 'react-markdown';
 import { useForm } from 'react-hook-form';
+import ReactMarkdown from 'react-markdown';
 
 const QuestionForm = ({formType, errors, question}) => {
-  const [newFormType, setFormType] = useState(formType);
+  const [questionTitle, setQuestionTitle] = useState(question.title);
+  const [questionBody, setQuestionBody] = useState(question.body);
+
 
   return (
     <div className='question-form-container'>
       <h1 className='questions-header question-form-header'>{formType}</h1>
-      <h2>{question.author_id.username}</h2>
       <form className='question-form' action="">
         <input
           className='question-form-title '
-          type="text"
           placeholder='Is there an R function for finding the index of an element in a vector?'
+          type="text"
         />
         <textarea className='question-form-body'></textarea>
+        <div className='question-form-md'>
+          <ReactMarkdown children={''} />
+        </div>
         <button className='question-form-submit' type="submit">Post Question</button>
       </form>
     </div>
@@ -23,4 +27,4 @@ const QuestionForm = ({formType, errors, question}) => {
 
 }
 
-export default QuestionForm
+export default QuestionForm;
