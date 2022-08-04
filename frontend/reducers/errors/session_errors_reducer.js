2bbe31b87da3d7f145b8191466a6c2dd78b8be44
Duncan Murchison <duncancmurchison@gmail.com>
2022-08-04 12:57:10 -0400

    sess_err_reducer

--- frontend/reducers/errors/session_errors_reducer.js
+++ frontend/reducers/errors/session_errors_reducer.js
@@ -1,6 +1,5 @@
 import {
   RECEIVE_SESSION_ERRORS,
-  RECEIVE_CURRENT_USER,
   REMOVE_SESSION_ERRORS,
 } from '../../actions/session_actions';
 
@@ -9,8 +8,6 @@ export const sessionErrorsReducer = (state = [], action) => {
   switch (action.type) {
     case RECEIVE_SESSION_ERRORS:
       return action.errors;
-    case RECEIVE_CURRENT_USER:
-      return [];
     case REMOVE_SESSION_ERRORS:
       return [];
     default:
