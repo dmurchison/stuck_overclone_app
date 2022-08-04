8013daa24a994ee2b5ee1965c281d52b17cf5226
Duncan Murchison <duncancmurchison@gmail.com>
2022-08-03 20:00:40 -0400

    h

--- config/application.rb
+++ config/application.rb
@@ -5,12 +5,12 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-config.assets.initialize_on_precompile = false
 
 module StuckOvercloneApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.assets.initialize_on_precompile = false
     
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
